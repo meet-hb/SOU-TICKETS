84c1ebd016d125d29cd78fc6e9396dd29bd886f2
MeetNayak06 <meetnayak6419@gmail.com>
2024-12-03 16:36:16 +0530

    init

--- SOU/js/main.js
+++ SOU/js/main.js
@@ -117,4 +117,21 @@ function readURL(input) {
 $("#imageUpload").change(function() {
     readURL(this);
 });
+$(document).ready(function() {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
+
 
