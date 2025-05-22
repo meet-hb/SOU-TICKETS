54fc6467c6cc2b350d2b5e8f3cfaace1c06ba6ed
MeetNayak06 <meetnayak6419@gmail.com>
2025-05-22 12:39:41 +0530

    new add

--- SOU/js/main.js
+++ SOU/js/main.js
@@ -1,193 +1,184 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner(0);
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner(0);
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.nav-bar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.nav-bar').removeClass('sticky-top shadow-sm');
-        }
-    });
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 5,
-        time: 2000
-    });
-
-
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
-
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
-    });
-
-
-    // Testimonial-carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 2000,
-        center: false,
-        dots: false,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:2
-            }
-        }
-    });
-
-    
-    
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 45) {
+      $(".nav-bar").addClass("sticky-top shadow-sm");
     } else {
-        $('.back-to-top').fadeOut('slow');
+      $(".nav-bar").removeClass("sticky-top shadow-sm");
     }
+  });
+
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 5,
+    time: 2000,
+  });
+
+  // Modal Video
+  $(document).ready(function () {
+    var $videoSrc;
+    $(".btn-play").click(function () {
+      $videoSrc = $(this).data("src");
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
+    console.log($videoSrc);
+
+    $("#videoModal").on("shown.bs.modal", function (e) {
+      $("#video").attr(
+        "src",
+        $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+      );
     });
 
+    $("#videoModal").on("hide.bs.modal", function (e) {
+      $("#video").attr("src", $videoSrc);
+    });
+  });
 
-   
+  // Testimonial-carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 2000,
+    center: false,
+    dots: false,
+    loop: true,
+    margin: 25,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 2,
+      },
+      1200: {
+        items: 2,
+      },
+    },
+  });
 
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 })(jQuery);
 
 function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
-            $('#imagePreview').hide();
-            $('#imagePreview').fadeIn(650);
-        }
-        reader.readAsDataURL(input.files[0]);
-    }
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $("#imagePreview").css(
+        "background-image",
+        "url(" + e.target.result + ")"
+      );
+      $("#imagePreview").hide();
+      $("#imagePreview").fadeIn(650);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
 }
-$("#imageUpload").change(function() {
-    readURL(this);
+$("#imageUpload").change(function () {
+  readURL(this);
 });
-$(document).ready(function() {
-    $('.minus').click(function () {
-        var $input = $(this).parent().find('input');
-        var count = parseInt($input.val()) - 1;
-        count = count < 1 ? 1 : count;
-        $input.val(count);
-        $input.change();
-        return false;
-    });
-    $('.plus').click(function () {
-        var $input = $(this).parent().find('input');
-        $input.val(parseInt($input.val()) + 1);
-        $input.change();
-        return false;
-    });
-});
-
-
-$("#FileInput").on('change',function (e) {
-    var labelVal = $(".title").text();
-    var oldfileName = $(this).val();
-        fileName = e.target.value.split( '\\' ).pop();
-
-        if (oldfileName == fileName) {return false;}
-        var extension = fileName.split('.').pop();
-
-    if ($.inArray(extension,['Excel']) >= 0) {
-        $(".filelabel i").removeClass().addClass('fa fa-file-excel-o');
-        $(".filelabel i, .filelabel .title").css({'color':'#208440'});
-        $(".filelabel").css({'border':' 2px solid #208440'});
-    }
-    
-
-    if(fileName ){
-        if (fileName.length > 10){
-            $(".filelabel .title").text(fileName.slice(0,4)+'...'+extension);
-        }
-        else{
-            $(".filelabel .title").text(fileName);
-        }
-    }
-    else{
-        $(".filelabel .title").text(labelVal);
-    }
-});
-$("#FileInput1").on('change',function (e) {
-    var labelVal = $(".title").text();
-    var oldfileName = $(this).val();
-        fileName = e.target.value.split( '\\' ).pop();
-
-        if (oldfileName == fileName) {return false;}
-        var extension = fileName.split('.').pop();
-
-    if ($.inArray(extension,['jpg','pdf','png']) >= 0) {
-        $(".filelabel i").removeClass().addClass('fa fa-file-image-o');
-        $(".filelabel i, .filelabel .title").css({'color':'#208440'});
-        $(".filelabel").css({'border':' 2px solid #208440'});
-    }
-    
-
-    if(fileName ){
-        if (fileName.length > 10){
-            $(".filelabel .title").text(fileName.slice(0,4)+'...'+extension);
-        }
-        else{
-            $(".filelabel .title").text(fileName);
-        }
-    }
-    else{
-        $(".filelabel .title").text(labelVal);
-    }
+$(document).ready(function () {
+  $(".minus").click(function () {
+    var $input = $(this).parent().find("input");
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $(".plus").click(function () {
+    var $input = $(this).parent().find("input");
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
 });
 
+$("#FileInput").on("change", function (e) {
+  var labelVal = $(".title").text();
+  var oldfileName = $(this).val();
+  fileName = e.target.value.split("\\").pop();
 
+  if (oldfileName == fileName) {
+    return false;
+  }
+  var extension = fileName.split(".").pop();
+
+  if ($.inArray(extension, ["Excel"]) >= 0) {
+    $(".filelabel i").removeClass().addClass("fa fa-file-excel-o");
+    $(".filelabel i, .filelabel .title").css({ color: "#208440" });
+    $(".filelabel").css({ border: " 2px solid #208440" });
+  }
+
+  if (fileName) {
+    if (fileName.length > 10) {
+      $(".filelabel .title").text(fileName.slice(0, 4) + "..." + extension);
+    } else {
+      $(".filelabel .title").text(fileName);
+    }
+  } else {
+    $(".filelabel .title").text(labelVal);
+  }
+});
+$("#FileInput1").on("change", function (e) {
+  var labelVal = $(".title").text();
+  var oldfileName = $(this).val();
+  fileName = e.target.value.split("\\").pop();
+
+  if (oldfileName == fileName) {
+    return false;
+  }
+  var extension = fileName.split(".").pop();
+
+  if ($.inArray(extension, ["jpg", "pdf", "png"]) >= 0) {
+    $(".filelabel i").removeClass().addClass("fa fa-file-image-o");
+    $(".filelabel i, .filelabel .title").css({ color: "#208440" });
+    $(".filelabel").css({ border: " 2px solid #208440" });
+  }
+
+  if (fileName) {
+    if (fileName.length > 10) {
+      $(".filelabel .title").text(fileName.slice(0, 4) + "..." + extension);
+    } else {
+      $(".filelabel .title").text(fileName);
+    }
+  } else {
+    $(".filelabel .title").text(labelVal);
+  }
+});
